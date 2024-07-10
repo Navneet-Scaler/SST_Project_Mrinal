c67aecae1aede6202780203a941111d826e9ab57
Navneet-Scaler <navneet.23bcs10136@ms.sst.scaler.com>
2024-07-10 10:23:14 +0530

    filles updated

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const User = require("../models/userModel");
-
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
 
+    try {
+
+        const newUser = new User(req.body)
+        await newUser.save()
+
+        res.status(201).json('User Created');
+
+    } catch (error) {
+        res.json(error);
+    }
 
 });
 
